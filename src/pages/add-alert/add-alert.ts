9fb00be2a0853765828561e8f28a9b1945143e01
funyug <shivamchawla243@gmail.com>
2017-11-23 19:21:42 +0530

    Added validation

--- src/pages/add-alert/add-alert.ts
+++ src/pages/add-alert/add-alert.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Http} from "@angular/http";
+import {AlertsPage} from "../alerts/alerts";
 
 /**
  * Generated class for the AddAlertPage page.
@@ -13,13 +15,42 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add-alert.html',
 })
 export class AddAlertPage {
-  alert: {  ExchangeId: number, AlertPrice: number, AlertType: number} = {
+  alert: {  ExchangeId: number, AlertPrice: number, AlertType: number, DeviceId:string} = {
     ExchangeId: null,
     AlertPrice: null,
-    AlertType: null
+    AlertType: null,
+    //DeviceId:localStorage.getItem("device_id")
+    DeviceId:"test123"
   };
+  error :string="";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
+  }
+
+  postAlert() {
+    if(this.alert.ExchangeId == null) {
+      this.error = "Please select an exchange";
+      return;
+    }
+    if(!this.alert.AlertPrice) {
+      this.error = "Please enter the price";
+      return;
+    }
+    if(this.alert.AlertType == null) {
+      this.error = "Please select the type";
+      return;
+    }
+    this.http.post(`http://localhost:3001/alerts`,this.alert)
+      .subscribe(data => {
+        data = data.json();
+        if(data['Success'] == 1) {
+          this.navCtrl.push(AlertsPage);
+        }
+        else {
+          this.error = data["Data"];
+          console.log(this.error);
+        }
+      });
   }
 
 
